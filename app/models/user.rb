5840d8410ce27d629f6293e7646e9f805d1897c7
tvquy17it3 <tvquy.17it3@sict.udn.vn>
2021-09-17 14:53:46 +0700

    Chapter 9, Advantage Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   validates :name, :email, :password, presence: true
   validates :name, length:
     {
@@ -22,5 +23,24 @@ class User < ApplicationRecord
               BCrypt::Engine.cost
             end
     BCrypt::Password.create string, cost: cost
+  end
+
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_column :remember_digest, nil
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_142707) do
-
+ActiveRecord::Schema.define(version: 2021_09_24_131743) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
