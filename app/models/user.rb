3fff382c05feb508b87ff53327d7c420776c4cfb
tvquy17it3 <tvquy.17it3@sict.udn.vn>
2021-09-17 14:53:46 +0700

    Chapter 8, Base Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,13 @@ class User < ApplicationRecord
     length: {maximum: Settings.length.max_250},
     format: {with: URI::MailTo::EMAIL_REGEXP}
   has_secure_password
+
+  def self.digest string
+    cost =  if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+            else
+              BCrypt::Engine.cost
+            end
+    BCrypt::Password.create string, cost: cost
+  end
 end
