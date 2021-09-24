5824c6432536287322aa0d27e0d78842eba9ff66
tvquy17it3 <tvquy.17it3@sict.udn.vn>
2021-09-24 10:30:05 +0700

    Chapter7 Sing Up

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
     get "/home", to: "static_pages#home"
     get "/help", to: "static_pages#help"
     get "/about", to: "static_pages#about"
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users, only: %i(new create show)
   end
 end
