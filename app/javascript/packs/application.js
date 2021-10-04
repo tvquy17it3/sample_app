6efb06c01767985829264215402bdf76cca7d504
tvquy17it3 <tvquy.17it3@sict.udn.vn>
2021-10-04 20:55:09 +0700

    chapter 13, Micropost

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
+import I18n from "i18n-js"
+
+window.I18n = I18n
 require("jquery")
 
 Rails.start()
---
+++ app/models/micropost.rb
@@ -0,0 +1,19 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  scope :recent_posts, ->{order created_at: :desc}
+  validates :user_id, presence: true
+  validates :content, presence: true, length: {maximum: Settings.length.max_250}
+  validates :image, content_type: {
+    in: Settings.format_img,
+    message: :valid_image_format
+  }, size: {
+    less_than: Settings.less_than_5.megabytes,
+    message: :less_than_5mb
+  }
+
+  def display_image
+    image.variant(resize_to_limit: Settings.img.size_arr_500)
+  end
+end
